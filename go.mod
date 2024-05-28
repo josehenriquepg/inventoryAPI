4788fb052a299fd2a162c7f0bc031ab421bd4cc0
josehenriquepg <hpereira012@gmail.com>
2024-05-28 13:28:29 -0300

    feat: data structure and implementation of CRUD endpoints

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module API-RESTful-products
+module inventoryAPI
 
 go 1.22.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name"`
@@ -8,6 +17,22 @@ type Product struct {
 	Quantity    int     `json:"quantity"`
 }
 
-func main() {
+var products []Product
+var nextID int
 
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/products", getProducts).Methods("GET")
+	r.HandleFunc("/products", createProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	r.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+
+	fmt.Println("Server running at http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(products)
 }
